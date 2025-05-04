25775a64503c65d2f8ec0a3195633131fbaccc57
黎泽懿 <lzy.12@foxmail.com>
2025-05-04 21:47:47 +0800

    fix：重启代码堵塞进程

--- app/view/SecRandom.py
+++ app/view/SecRandom.py
@@ -360,37 +360,42 @@ class Window(MSFluentWindow):
         self.hide()
         self.tray_icon.hide()
 
-        if hasattr(self, 'server'):
-            self.server.close()
+        root_dir = os.path.abspath()
+        cmd_path = os.path.join(root_dir, "SecRandom_restart.cmd")
+        cmd_path_re = os.path.join(root_dir, "SecRandom_re.cmd")
 
-        temp_dir = "app/resource/TEMP_"
-        if not os.path.exists(temp_dir):
-            os.makedirs(temp_dir)
+        cmd_content_re = f"""@echo off
+set __COMPAT_LAYER=RunAsInvoker
+start "" /B "{cmd_path}" #
+del "%~f0"
+"""
 
         cmd_content = f"""@echo off
-TIMEOUT /T 3
+TIMEOUT /T 1
 set __COMPAT_LAYER=RunAsInvoker
 start "" /B "{sys.executable}" {" ".join(sys.argv)}
 del "%~f0"
 """
-        cmd_path = "app/resource/TEMP_/SecRandom_restart.cmd"
-        # current_path = os.path.abspath(sys.argv[0])
 
         try:
             logger.debug("正在通过cmd脚本重启程序...")
             logger.remove()
             with open(cmd_path, "w") as f:
                 f.write(cmd_content)
+            with open(cmd_path_re, "w") as f:
+                f.write(cmd_content_re)
             self.start_cleanup()
             logger.remove()
             if hasattr(self, 'server'):
                 self.server.close()
-            subprocess.Popen(cmd_path, creationflags=subprocess.CREATE_NEW_CONSOLE)
+            subprocess.Popen(cmd_path_re, creationflags=subprocess.CREATE_NEW_CONSOLE)
             QApplication.quit()
 
         except Exception as e:
             logger.error(f"创建重启脚本失败: {e},使用 os.execl(sys.executable, sys.executable, *sys.argv) 重启程序")
             logger.remove()
+            if hasattr(self, 'server'):
+                self.server.close()
             os.execl(sys.executable, sys.executable, *sys.argv)
 
     def show_setting_interface(self):
