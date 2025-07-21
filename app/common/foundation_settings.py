bc7e05bfd2c420baf4fc4bbacb319e6d1ae09c34
黎泽懿 <lzy.12@foxmail.com>
2025-07-21 16:51:59 +0800

    取消url注册功能

--- app/common/foundation_settings.py
+++ app/common/foundation_settings.py
@@ -117,7 +117,7 @@ class foundation_settingsCard(GroupHeaderCardWidget):
 
         # 添加组件到分组中
         self.addGroup(get_theme_icon("ic_fluent_branch_compare_20_filled"), "开机自启", "系统启动时自动启动本应用(启用后将自动设置不显示主窗口)", self.self_starting_switch)
-        self.addGroup(get_theme_icon("ic_fluent_branch_fork_link_20_filled"), "URL协议注册", "允许其他程序通过secrandom://协议调用本应用", self.url_protocol_switch)
+        # self.addGroup(get_theme_icon("ic_fluent_branch_fork_link_20_filled"), "URL协议注册", "允许其他程序通过secrandom://协议调用本应用", self.url_protocol_switch)
         self.addGroup(get_theme_icon("ic_fluent_window_ad_20_filled"), "浮窗显隐", "设置便捷抽人的浮窗显示/隐藏", self.pumping_floating_switch)
         self.addGroup(get_theme_icon("ic_fluent_arrow_autofit_height_20_filled"), "抽人选项侧边栏位置", "设置抽人选项侧边栏位置", self.pumping_floating_side_comboBox)
         self.addGroup(get_theme_icon("ic_fluent_arrow_autofit_height_20_filled"), "抽奖选项侧边栏位置", "设置抽奖选项侧边栏位置", self.pumping_reward_side_comboBox)
