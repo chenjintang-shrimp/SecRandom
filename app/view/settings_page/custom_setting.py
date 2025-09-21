38ee29780b6b8eb20a66dc81af3e0d24238cfa46
黎泽懿 <lzy.12@foxmail.com>
2025-09-21 12:48:54 +0800

    新增多个个性化页面

--- app/view/settings_page/custom_setting.py
+++ app/view/settings_page/custom_setting.py
@@ -9,7 +9,11 @@ from app.common.fixed_url_setting import fixed_url_SettinsCard
 # from app.common.user_defined_url_setting import user_defined_url_SettinsCard
 from app.common.theme_settings import theme_settingsCard
 from app.common.sidebar_settings import sidebar_settingsCard
-# from app.common.Program_functionality_settings import Program_functionality_settingsCard
+from app.common.floating_window_settings import floating_window_settingsCard
+from app.common.roll_call_settings import roll_call_settingsCard
+from app.common.rewards_settings import reward_settingsCard
+from app.common.instant_draw_settings import instant_draw_settingsCard
+from app.common.Program_functionality_settings import Program_functionality_settingsCard
 
 class custom_setting(QFrame):
     def __init__(self, parent: QFrame = None):
@@ -24,14 +28,18 @@ class custom_setting(QFrame):
         self.user_defined_url_page = QWidget()
         self.theme_settings_page = QWidget()
         self.sidebar_settings_page = QWidget()
+        self.floating_window_page = QWidget()
+        self.extraction_management_page = QWidget()
         self.Program_functionality_settings_page = QWidget()
         
         # 添加子页面
+        self.addSubInterface(self.extraction_management_page, 'extraction_management', '抽取管理设置')
+        self.addSubInterface(self.sidebar_settings_page, 'sidebar_settings', '侧边栏设置')
+        self.addSubInterface(self.floating_window_page, 'floating_window', '浮窗管理设置')
         self.addSubInterface(self.fixed_url_page, 'fixed_url_setting', '固定Url管理')
         # self.addSubInterface(self.user_defined_url_page, 'user_defined_url', '自定义Url')
         self.addSubInterface(self.theme_settings_page, 'theme_settings', '主题设置')
-        self.addSubInterface(self.sidebar_settings_page, 'sidebar_settings', '侧边栏管理')
-        # self.addSubInterface(self.Program_functionality_settings_page, 'Program_functionality_settings', '软件功能管理')
+        self.addSubInterface(self.Program_functionality_settings_page, 'Program_functionality_settings', '软件功能管理')
 
         # 固定Url设置
         # 创建滚动区域
@@ -149,34 +157,115 @@ class custom_setting(QFrame):
         sidebar_settings_layout = QVBoxLayout(self.sidebar_settings_page)
         sidebar_settings_layout.addWidget(sidebar_settings_scroll_area)
 
-        # # 软件功能设置
-        # # 创建滚动区域
-        # Program_functionality_settings_scroll_area = SingleDirectionScrollArea(self.Program_functionality_settings_page)
-        # Program_functionality_settings_scroll_area.setWidgetResizable(True)
-        # # 设置样式表
-        # Program_functionality_settings_scroll_area.setStyleSheet("""
-        #     QScrollArea {
-        #         border: none;
-        #         background-color: transparent;
-        #     }
-        #     QScrollArea QWidget {
-        #         border: none;
-        #         background-color: transparent;
-        #     }
-        # """)
-        # # 启用鼠标滚轮
-        # QScroller.grabGesture(Program_functionality_settings_scroll_area.viewport(), QScroller.LeftMouseButtonGesture)
-        # # 创建内部框架
-        # Program_functionality_settings_inner_frame = QWidget(Program_functionality_settings_scroll_area)
-        # Program_functionality_settings_inner_layout = QVBoxLayout(Program_functionality_settings_inner_frame)
-        # Program_functionality_settings_inner_layout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
-        # Program_functionality_settings_scroll_area.setWidget(Program_functionality_settings_inner_frame)
-        # # 软件功能设置卡片组
-        # Program_functionality_settings_card = Program_functionality_settingsCard()
-        # Program_functionality_settings_inner_layout.addWidget(Program_functionality_settings_card)
-        # # 设置软件功能设置页面布局
-        # Program_functionality_settings_layout = QVBoxLayout(self.Program_functionality_settings_page)
-        # Program_functionality_settings_layout.addWidget(Program_functionality_settings_scroll_area)
+        # 浮窗管理设置
+        # 创建滚动区域
+        floating_window_scroll_area = SingleDirectionScrollArea(self.floating_window_page)
+        floating_window_scroll_area.setWidgetResizable(True)
+        # 设置样式表
+        floating_window_scroll_area.setStyleSheet("""
+            QScrollArea {
+                border: none;
+                background-color: transparent;
+            }
+            QScrollArea QWidget {
+                border: none;
+                background-color: transparent;
+            }
+        """)
+        # 启用鼠标滚轮
+        QScroller.grabGesture(floating_window_scroll_area.viewport(), QScroller.LeftMouseButtonGesture)
+        # 创建内部框架
+        floating_window_inner_frame = QWidget(floating_window_scroll_area)
+        floating_window_inner_layout = QVBoxLayout(floating_window_inner_frame)
+        floating_window_inner_layout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        floating_window_scroll_area.setWidget(floating_window_inner_frame)
+        # 浮窗管理设置卡片组
+        floating_window_card = floating_window_settingsCard()
+        floating_window_inner_layout.addWidget(floating_window_card)
+        # 设置浮窗管理设置页面布局
+        floating_window_layout = QVBoxLayout(self.floating_window_page)
+        floating_window_layout.addWidget(floating_window_scroll_area)
+
+        # 抽取管理设置
+        # 创建滚动区域
+        extraction_management_scroll_area = SingleDirectionScrollArea(self.extraction_management_page)
+        extraction_management_scroll_area.setWidgetResizable(True)
+        # 设置样式表
+        extraction_management_scroll_area.setStyleSheet("""
+            QScrollArea {
+                border: none;
+                background-color: transparent;
+            }
+            QScrollArea QWidget {
+                border: none;
+                background-color: transparent;
+            }
+        """)
+        # 启用鼠标滚轮
+        QScroller.grabGesture(extraction_management_scroll_area.viewport(), QScroller.LeftMouseButtonGesture)
+        # 创建内部框架
+        extraction_management_inner_frame = QWidget(extraction_management_scroll_area)
+        extraction_management_inner_layout = QVBoxLayout(extraction_management_inner_frame)
+        extraction_management_inner_layout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        extraction_management_scroll_area.setWidget(extraction_management_inner_frame)
+        
+        # 创建分隔线
+        separator1 = QFrame()
+        separator1.setFrameShape(QFrame.HLine)
+        separator1.setFrameShadow(QFrame.Sunken)
+        separator1.setStyleSheet("background-color: rgba(0, 0, 0, 0.1); margin: 10px 0;")
+        
+        separator2 = QFrame()
+        separator2.setFrameShape(QFrame.HLine)
+        separator2.setFrameShadow(QFrame.Sunken)
+        separator2.setStyleSheet("background-color: rgba(0, 0, 0, 0.1); margin: 10px 0;")
+        
+        # 点名界面设置卡片组
+        roll_call_card = roll_call_settingsCard()
+        extraction_management_inner_layout.addWidget(roll_call_card)
+        extraction_management_inner_layout.addWidget(separator1)
+        
+        # 抽奖界面设置卡片组
+        reward_card = reward_settingsCard()
+        extraction_management_inner_layout.addWidget(reward_card)
+        extraction_management_inner_layout.addWidget(separator2)
+        
+        # 即抽管理设置卡片组
+        instant_draw_card = instant_draw_settingsCard()
+        extraction_management_inner_layout.addWidget(instant_draw_card)
+        
+        # 设置抽取管理设置页面布局
+        extraction_management_layout = QVBoxLayout(self.extraction_management_page)
+        extraction_management_layout.addWidget(extraction_management_scroll_area)
+
+        # 软件功能设置
+        # 创建滚动区域
+        Program_functionality_settings_scroll_area = SingleDirectionScrollArea(self.Program_functionality_settings_page)
+        Program_functionality_settings_scroll_area.setWidgetResizable(True)
+        # 设置样式表
+        Program_functionality_settings_scroll_area.setStyleSheet("""
+            QScrollArea {
+                border: none;
+                background-color: transparent;
+            }
+            QScrollArea QWidget {
+                border: none;
+                background-color: transparent;
+            }
+        """)
+        # 启用鼠标滚轮
+        QScroller.grabGesture(Program_functionality_settings_scroll_area.viewport(), QScroller.LeftMouseButtonGesture)
+        # 创建内部框架
+        Program_functionality_settings_inner_frame = QWidget(Program_functionality_settings_scroll_area)
+        Program_functionality_settings_inner_layout = QVBoxLayout(Program_functionality_settings_inner_frame)
+        Program_functionality_settings_inner_layout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        Program_functionality_settings_scroll_area.setWidget(Program_functionality_settings_inner_frame)
+        # 软件功能设置卡片组
+        Program_functionality_settings_card = Program_functionality_settingsCard()
+        Program_functionality_settings_inner_layout.addWidget(Program_functionality_settings_card)
+        # 设置软件功能设置页面布局
+        Program_functionality_settings_layout = QVBoxLayout(self.Program_functionality_settings_page)
+        Program_functionality_settings_layout.addWidget(Program_functionality_settings_scroll_area)
         
         # 设置主布局
         main_layout = QVBoxLayout(self)
@@ -185,8 +274,8 @@ class custom_setting(QFrame):
         main_layout.addWidget(self.SegmentedWidget, 0, Qt.AlignHCenter)
         main_layout.addWidget(self.stackedWidget)
         
-        self.stackedWidget.setCurrentWidget(self.fixed_url_page)
-        self.SegmentedWidget.setCurrentItem('fixed_url_setting')
+        self.stackedWidget.setCurrentWidget(self.extraction_management_page)
+        self.SegmentedWidget.setCurrentItem('extraction_management')
 
         self.__connectSignalToSlot()
         
